4301c428f643df2a4cb0afeec95a2ac2279076bd
abdul razak shaikh <abdul.shaikh@futurescapetech.com>
2022-10-07 21:41:41 +0530

    dashboard done

--- lib/homepage/homepage_categories.dart
+++ lib/homepage/homepage_categories.dart
@@ -15,38 +15,43 @@ class _HomepageCategoriesState extends State<HomepageCategories> {
 List carouselitemlist = [
   {
     "id": "001",
-    "title" : "Lorem Ipsum",
-    "category": "Categrory",
-    "img": "assets/images/bg1.jpg",
-    "rating": 3.5,
+    "title" : "Tutor",
+    "img": "assets/images/tutor.png",
   },
   {
     "id": "002",
-    "title" : "Lorem Ipsum",
-    "category": "Categrory",
-    "img": "assets/images/bg1.jpg",
-    "rating": 3.5,
+    "title" : "Projects",
+    "img": "assets/images/project.png",
   },
   {
     "id": "003",
-    "title" : "Lorem Ipsum",
-    "category": "Categrory",
-    "img": "assets/images/bg1.jpg",
-    "rating": 3.5,
+    "title" : "Activity",
+    "img": "assets/images/activity.png",
   },
   {
     "id": "004",
-    "title" : "Lorem Ipsum",
-    "category": "Categrory",
-    "img": "assets/images/bg1.jpg",
-    "rating": 3.5,
+    "title" : "Coach",
+    "img": "assets/images/coach.png",
   },
   {
     "id": "005",
-    "title" : "Lorem Ipsum",
-    "category": "Categrory",
-    "img": "assets/images/bg1.jpg",
-    "rating": 3.5,
+    "title" : "Charts",
+    "img": "assets/images/chart.png",
+  },
+  {
+    "id": "006",
+    "title" : "Models",
+    "img": "assets/images/model.png",
+  },
+  {
+    "id": "007",
+    "title" : "Sports",
+    "img": "assets/images/sports.png",
+  },
+  {
+    "id": "008",
+    "title" : "Assignments",
+    "img": "assets/images/assignment.png",
   },
 ];
 
@@ -57,18 +62,18 @@ List carouselitemlist = [
     child: Column(
       children: [
         Container(
-          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Expanded(
-                child: Text('Categories',
+                child: Text('Categories'.toUpperCase(),
                   style: GoogleFonts.lato(
                     textStyle: Theme.of(context).textTheme.titleLarge,
-                    letterSpacing: 1.2,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
+                    letterSpacing: 1.5,
+                    fontSize: MediaQuery.of(context).size.width < 321 ? 16 : 18,
+                    fontWeight: FontWeight.w900
                   ),
                 )
               ),
@@ -81,12 +86,13 @@ List carouselitemlist = [
                         style: GoogleFonts.lato(
                           textStyle: Theme.of(context).textTheme.titleSmall,
                           letterSpacing: 1.2,
+                          fontSize: MediaQuery.of(context).size.width < 321 ? 12 : 14,
                           color: Theme.of(context).colorScheme.secondary,
                           fontWeight: FontWeight.w600
                         ),
                       ),
                       SizedBox(width: 5),
-                      Icon(Icons.arrow_forward, size: 18,), 
+                      Icon(Icons.arrow_forward, size: 16,), 
                     ],
                   ),
                   style: TextButton.styleFrom(
@@ -104,10 +110,11 @@ List carouselitemlist = [
 
           Container(
             alignment: Alignment.topLeft,
-            height: MediaQuery.of(context).size.width < 321 ?  280 : 260,
+            // height: MediaQuery.of(context).size.width < 321 ?  280 : 260,
+            height: 120,
             child: PageView.builder(
                 scrollDirection: Axis.horizontal,    
-                controller: PageController(viewportFraction: 0.6,), 
+                controller: PageController(viewportFraction: MediaQuery.of(context).size.width < 321 ? 0.7 : 0.6,), 
                 padEnds: false,
                 pageSnapping: true,
                 itemCount: carouselitemlist == null ? 0 : carouselitemlist.length,
@@ -122,66 +129,39 @@ List carouselitemlist = [
                         elevation: 5,
                         shadowColor: Theme.of(context).shadowColor.withOpacity(0.35),
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
+                          borderRadius: BorderRadius.circular(15),
                           side: BorderSide(width: 1, color: Colors.grey.withOpacity(0.2))
                         ),
                         child: Padding(
                           padding: EdgeInsets.all(10),
-                          child: Column(
+                          child: Row(
                             children: [
                               Container(
-                                height: 130,
+                                width: 64,
+                                // height: 84,
+                                padding: EdgeInsets.all(10),
                                 decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(20),
+                                  color: Theme.of(context).colorScheme.tertiary.withOpacity(0.1),
+                                  borderRadius: BorderRadius.circular(10),
                                 ),
-                                width: double.infinity,
                                 child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(15),
+                                  borderRadius: BorderRadius.circular(10),
                                   child: Image.asset('${item["img"]}',
-                                    fit: BoxFit.cover,
+                                    fit: BoxFit.contain,
                                   ),
                                 ),
                               ),
-                              Container(
-                                padding: EdgeInsets.only(top: 10),
-                                width: double.infinity,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                                  children: [
-                                    Text('${item["title"]}',
-                                      overflow: TextOverflow.clip,
-                                      style: GoogleFonts.lato(
-                                        textStyle: Theme.of(context).textTheme.titleMedium,
-                                        letterSpacing: 1.2,
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.w700,
-                                      ),
-                                    ),
-                                    SizedBox(height: 5),
-                                    Text('${item["category"]}',
-                                      overflow: TextOverflow.clip,
-                                      style: GoogleFonts.lato(
-                                        textStyle: Theme.of(context).textTheme.bodySmall,
-                                        color: Theme.of(context).colorScheme.secondary,
-                                        letterSpacing: 1.2,
-                                        fontWeight: FontWeight.w400
-                                      ),
-                                    ),
-                                    SizedBox(height: 5),
-                                    RatingBarIndicator(
-                                      rating: item["rating"],
-                                      
-                                      itemBuilder: (context, index) => Icon(
-                                          Icons.star,
-                                          color: Colors.amber,
-                                      ),
-                                      itemCount: 5,
-                                      itemSize: 16,
-                                      direction: Axis.horizontal,
-                                    ),
-                                  ],
+                              SizedBox(width: 10),
+                              Expanded(
+                                child: Text('${item["title"]}',
+                                  overflow: TextOverflow.clip,
+                                  style: GoogleFonts.lato(
+                                    textStyle: Theme.of(context).textTheme.titleMedium,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600,
+                                  ),
                                 ),
-                              )
+                              ),
                             ],
                           ),
                         ),
@@ -192,99 +172,6 @@ List carouselitemlist = [
               
           ),  
         ),
-
-        // Container(
-        //   height: MediaQuery.of(context).size.width < 321 ?  280 : 260,
-        //   child: Swiper(
-        //     physics: const PageScrollPhysics(),
-        //     itemBuilder: (BuildContext context, int index) {
-        //       Map item = carouselitemlist[index];
-        //       return Container(
-        //       // transform: Transform.translate( offset: const Offset(-15, 0), ).transform,
-        //       margin: const EdgeInsets.only( bottom: 25 ), 
-        //       // margin: EdgeInsets.only(bottom: 25),
-        //       child: GestureDetector(
-        //         onTap: () {},
-        //           child: Card(
-        //             elevation: 5,
-        //             shadowColor: Theme.of(context).shadowColor.withOpacity(0.35),
-        //             shape: RoundedRectangleBorder(
-        //               borderRadius: BorderRadius.circular(20),
-        //               side: BorderSide(width: 1, color: Colors.grey.withOpacity(0.2))
-        //             ),
-        //             child: Padding(
-        //               padding: EdgeInsets.all(10),
-        //               child: Column(
-        //                 children: [
-        //                   Container(
-        //                     height: 130,
-        //                     decoration: BoxDecoration(
-        //                       borderRadius: BorderRadius.circular(20),
-        //                     ),
-        //                     width: double.infinity,
-        //                     child: ClipRRect(
-        //                       borderRadius: BorderRadius.circular(15),
-        //                       child: Image.asset('${item["img"]}',
-        //                         fit: BoxFit.cover,
-        //                       ),
-        //                     ),
-        //                   ),
-        //                   Container(
-        //                     padding: EdgeInsets.only(top: 10),
-        //                     width: double.infinity,
-        //                     child: Column(
-        //                       crossAxisAlignment: CrossAxisAlignment.stretch,
-        //                       children: [
-        //                         Text('${item["title"]}',
-        //                           overflow: TextOverflow.clip,
-        //                           style: GoogleFonts.lato(
-        //                             textStyle: Theme.of(context).textTheme.titleMedium,
-        //                             letterSpacing: 1.2,
-        //                             fontSize: 18,
-        //                             fontWeight: FontWeight.w700,
-        //                           ),
-        //                         ),
-        //                         SizedBox(height: 5),
-        //                         Text('${item["category"]}',
-        //                           overflow: TextOverflow.clip,
-        //                           style: GoogleFonts.lato(
-        //                             textStyle: Theme.of(context).textTheme.bodySmall,
-        //                             color: Theme.of(context).colorScheme.secondary,
-        //                             letterSpacing: 1.2,
-        //                             fontWeight: FontWeight.w400
-        //                           ),
-        //                         ),
-        //                         SizedBox(height: 5),
-        //                         RatingBarIndicator(
-        //                           rating: item["rating"],
-                                  
-        //                           itemBuilder: (context, index) => Icon(
-        //                               Icons.star,
-        //                               color: Colors.amber,
-        //                           ),
-        //                           itemCount: 5,
-        //                           itemSize: 16,
-        //                           direction: Axis.horizontal,
-        //                         ),
-        //                       ],
-        //                     ),
-        //                   )
-        //                 ],
-        //               ),
-        //             ),
-        //           ),
-        //         ),    
-        //       );
-        //     },
-        //     autoplay: false,
-        //     viewportFraction: MediaQuery.of(context).size.width < 321 ?  0.9 : 0.5,
-        //     itemCount: carouselitemlist == null ? 0 : carouselitemlist.length,
-        //     scrollDirection: Axis.horizontal,
-        //     loop: false,
-        //     control: null,
-        //   ),
-        // ),
-
 
       ],
     ),
--- lib/homepage/homepage_populargigs.dart
+++ lib/homepage/homepage_populargigs.dart
@@ -57,18 +57,18 @@ List carouselitemlist = [
     child: Column(
       children: [
         Container(
-          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Expanded(
-                child: Text('Popular Gigs',
+                child: Text('Popular Gigs'.toUpperCase(),
                   style: GoogleFonts.lato(
                     textStyle: Theme.of(context).textTheme.titleLarge,
-                    letterSpacing: 1.2,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
+                    letterSpacing: 1.5,
+                    fontSize: MediaQuery.of(context).size.width < 321 ? 16 : 18,
+                    fontWeight: FontWeight.w900
                   ),
                 )
               ),
@@ -81,12 +81,13 @@ List carouselitemlist = [
                         style: GoogleFonts.lato(
                           textStyle: Theme.of(context).textTheme.titleSmall,
                           letterSpacing: 1.2,
+                          fontSize: MediaQuery.of(context).size.width < 321 ? 12 : 14,
                           color: Theme.of(context).colorScheme.secondary,
                           fontWeight: FontWeight.w600
                         ),
                       ),
                       SizedBox(width: 5),
-                      Icon(Icons.arrow_forward, size: 18,), 
+                      Icon(Icons.arrow_forward, size: 16,), 
                     ],
                   ),
                   style: TextButton.styleFrom(
@@ -104,7 +105,7 @@ List carouselitemlist = [
                   
         Container(
                 alignment: Alignment.topLeft,
-                height: MediaQuery.of(context).size.width < 321 ?  280 : 260,
+                height: MediaQuery.of(context).size.width < 321 ?  260 : 260,
                 child: PageView.builder(
                 scrollDirection: Axis.horizontal,    
                 controller: PageController(viewportFraction: 0.85,), 
@@ -152,9 +153,8 @@ List carouselitemlist = [
                                   overflow: TextOverflow.clip,
                                   style: GoogleFonts.lato(
                                     textStyle: Theme.of(context).textTheme.titleMedium,
-                                    letterSpacing: 1.2,
                                     fontSize: 18,
-                                    fontWeight: FontWeight.w700,
+                                    fontWeight: FontWeight.w600,
                                   ),
                                 ),
                                 SizedBox(height: 5),
