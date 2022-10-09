a69faa4d6215e6e8a71de4ee2b663d6650048c0e
Vistar Mishra <vistar.mishra@futurescapetech.com>
2022-10-09 13:11:20 +0530

    Merge branch 'main' of https://github.com/abdulrazakshaikh/trackmymentor
    # Conflicts:
    #	.dart_tool/package_config.json
    #	.dart_tool/package_config_subset
    #	.flutter-plugins-dependencies
    #	build/c075001b96339384a97db4862b8ab8db.cache.dill.track.dill

--- lib/add/add.dart
+++ lib/add/add.dart
@@ -27,121 +27,123 @@ class _AddState extends State<Add> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CoolStepper(
-        isHeaderEnabled: false,
-        hasRoundedCorner: false,
-        onCompleted: (){
-          Navigator.push(
-            context, MaterialPageRoute(
-              builder: (BuildContext context) => Login()
-            )
-          );
-        },
-        contentPadding: EdgeInsets.all(10),
-        config: CoolStepperConfig(
-          backButton: Container(
-            padding: EdgeInsets.all(5),
-            child: TextButton(
-              onPressed: (){}, 
-              child: Row(
-                children: [
-                  Icon(Icons.arrow_back, color: Theme.of(context).colorScheme.secondary),
-                  SizedBox(width: 5),
-                  Text('Back'.toUpperCase(),
-                  style: GoogleFonts.lato(
-                    textStyle: Theme.of(context).textTheme.titleSmall,
-                    color: Theme.of(context).colorScheme.secondary,
-                    fontWeight: FontWeight.w700,
-                    letterSpacing: 1.2,
-                  ),
-                  ),
-                ],
-              ),
-              style: TextButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                alignment: Alignment.center,
-                foregroundColor: Theme.of(context).colorScheme.secondary,
-              ),
-            ),
-          ),
-          nextButton: Container(
-            padding: EdgeInsets.all(5),
-            child: ElevatedButton(
-              onPressed: (){}, 
-              child: Row(
-                children: [
-                  Text('Next'.toUpperCase(),
-                  style: GoogleFonts.lato(
-                    textStyle: Theme.of(context).textTheme.titleSmall,
-                    color: Theme.of(context).colorScheme.onPrimary,
-                    fontWeight: FontWeight.w700,
-                    letterSpacing: 1.2,
-                  ),
-                  ),
-                  SizedBox(width: 5),
-                  Icon(Icons.arrow_forward, color: Theme.of(context).colorScheme.onPrimary,)
-                ],
-              ),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                alignment: Alignment.center,
+    return SafeArea(
+      child: Scaffold(
+        body: CoolStepper(
+          isHeaderEnabled: false,
+          hasRoundedCorner: false,
+          onCompleted: (){
+            Navigator.push(
+              context, MaterialPageRoute(
+                builder: (BuildContext context) => Login()
+              )
+            );
+          },
+          contentPadding: EdgeInsets.all(10),
+          config: CoolStepperConfig(
+            backButton: Container(
+              padding: EdgeInsets.all(5),
+              child: TextButton(
+                onPressed: (){},
+                child: Row(
+                  children: [
+                    Icon(Icons.arrow_back, color: Theme.of(context).colorScheme.secondary),
+                    SizedBox(width: 5),
+                    Text('Back'.toUpperCase(),
+                    style: GoogleFonts.lato(
+                      textStyle: Theme.of(context).textTheme.titleSmall,
+                      color: Theme.of(context).colorScheme.secondary,
+                      fontWeight: FontWeight.w700,
+                      letterSpacing: 1.2,
+                    ),
+                    ),
+                  ],
+                ),
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                  alignment: Alignment.center,
+                  foregroundColor: Theme.of(context).colorScheme.secondary,
+                ),
               ),
             ),
-          ),
-          finishButton: Container(
-            padding: EdgeInsets.all(5),
-            child: ElevatedButton(
-              onPressed: (){}, 
-              child: Row(
-                children: [
-                  Text('Submit'.toUpperCase(),
+            nextButton: Container(
+              padding: EdgeInsets.all(5),
+              child: ElevatedButton(
+                onPressed: (){},
+                child: Row(
+                  children: [
+                    Text('Next'.toUpperCase(),
                     style: GoogleFonts.lato(
                       textStyle: Theme.of(context).textTheme.titleSmall,
                       color: Theme.of(context).colorScheme.onPrimary,
                       fontWeight: FontWeight.w700,
                       letterSpacing: 1.2,
                     ),
-                  ),
-                  SizedBox(width: 5),
-                  Icon(Icons.send, color: Theme.of(context).colorScheme.onPrimary,),
-                ],
-              ),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                alignment: Alignment.center,
+                    ),
+                    SizedBox(width: 5),
+                    Icon(Icons.arrow_forward, color: Theme.of(context).colorScheme.onPrimary,)
+                  ],
+                ),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                  alignment: Alignment.center,
+                ),
               ),
             ),
+            finishButton: Container(
+              padding: EdgeInsets.all(5),
+              child: ElevatedButton(
+                onPressed: (){},
+                child: Row(
+                  children: [
+                    Text('Submit'.toUpperCase(),
+                      style: GoogleFonts.lato(
+                        textStyle: Theme.of(context).textTheme.titleSmall,
+                        color: Theme.of(context).colorScheme.onPrimary,
+                        fontWeight: FontWeight.w700,
+                        letterSpacing: 1.2,
+                      ),
+                    ),
+                    SizedBox(width: 5),
+                    Icon(Icons.send, color: Theme.of(context).colorScheme.onPrimary,),
+                  ],
+                ),
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                  alignment: Alignment.center,
+                ),
+              ),
+            ),
+            stepColor: Theme.of(context).colorScheme.background,
+            headerColor: Theme.of(context).colorScheme.tertiary.withOpacity(0.1),
+            stepOfTextStyle: GoogleFonts.lato(
+              textStyle: Theme.of(context).textTheme.labelSmall,
+              letterSpacing: 1.5,
+              fontWeight: FontWeight.w500,
+            ),
+            titleTextStyle: GoogleFonts.lato(
+              textStyle: Theme.of(context).textTheme.headlineSmall,
+              letterSpacing: 1.5,
+              fontSize: 22,
+              fontWeight: FontWeight.w700,
+            ),
+            subtitleTextStyle: GoogleFonts.lato(
+              textStyle: Theme.of(context).textTheme.titleSmall,
+              letterSpacing: 1.5,
+            ),
           ),
-          stepColor: Theme.of(context).colorScheme.background,
-          headerColor: Theme.of(context).colorScheme.tertiary.withOpacity(0.1),
-          stepOfTextStyle: GoogleFonts.lato(
-            textStyle: Theme.of(context).textTheme.labelSmall,
-            letterSpacing: 1.5,
-            fontWeight: FontWeight.w500,
-          ),
-          titleTextStyle: GoogleFonts.lato(
-            textStyle: Theme.of(context).textTheme.headlineSmall,
-            letterSpacing: 1.5,
-            fontSize: 22,
-            fontWeight: FontWeight.w700,
-          ),
-          subtitleTextStyle: GoogleFonts.lato(
-            textStyle: Theme.of(context).textTheme.titleSmall,
-            letterSpacing: 1.5,
-          ),
+          steps: [
+            CoolStep(
+              content: AddStepOne()
+            ),
+            CoolStep(
+              content: AddStepTwo()
+            ),
+            CoolStep(
+              content: AddStepThree()
+            ),
+          ]
         ),
-        steps: [
-          CoolStep(
-            content: AddStepOne()
-          ),
-          CoolStep(
-            content: AddStepTwo()
-          ),
-          CoolStep(
-            content: AddStepThree()
-          ),
-        ]
       ),
     );
   }
